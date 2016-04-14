ee97b44e5f4b45f8786acffffa45971c48f692cc
cph57689 <pernillejaco@gmail.com>
2016-04-14 22:08:36 +0200

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Pernille on 14-04-2016.
+ */
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+/*
+app.get('/', function(req, res){
+    res.send('<h1>Hello world</h1>');
+});
+*/
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+io.on('connection', function(socket){
+    console.log('a user connected');
+    socket.on('disconnect', function(){
+        console.log('user disconnected');
+    });
+});
+//meddelelse fra browser sendes til alle sockets
+io.on('connection', function(socket){
+    socket.on('chat message', function(msg){
+        io.emit('chat message', msg);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
